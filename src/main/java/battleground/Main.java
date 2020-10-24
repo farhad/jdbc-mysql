1ae8c151845039ce7199cfed825391db9e7e7052
Farhad <ifarhadfaghihi@gmail.com>
2020-10-24 16:11:26 -0600

    added jdbc mysql connector to maven successfully connected to sql_store database on mysql

--- src/main/java/battleground/Main.java
+++ src/main/java/battleground/Main.java
@@ -1,8 +1,28 @@
 package battleground;
 
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) {
-       
+
+        String url = "jdbc:mysql://localhost:3306/sql_store?serverTimezone=UTC";
+        String userName = "root";
+        String password = "lifeisvoid";
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM customers");
+
+            while (resultSet.next()) {
+                String firstName = resultSet.getString(2);
+                String lastName = resultSet.getString(3);
+
+                System.out.println(firstName + " " + lastName);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
