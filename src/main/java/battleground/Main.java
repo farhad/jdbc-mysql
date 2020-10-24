77bf17c9b41cfa57814321b6efaec8fac179b0fc
Farhad <ifarhadfaghihi@gmail.com>
2020-10-24 17:11:46 -0600

    added update statement and get customerId after insert

--- src/main/java/battleground/Main.java
+++ src/main/java/battleground/Main.java
@@ -22,6 +22,14 @@ public class Main {
         customer.setState("AB");
         insertCustomer(connection, customer);
 
+        customer.setCustomerId(getCustomerId(connection, customer));
+
+        displayAllCustomers(connection);
+
+        customer.setFirstName("Tony");
+        customer.setLastName("Ezekiel");
+        updateCustomer(connection, customer);
+
         displayAllCustomers(connection);
 
         deleteCustomer(connection, customer);
@@ -30,11 +38,11 @@ public class Main {
     }
 
     private static void displayAllCustomers(Connection connection) {
-        try {
-            System.out.println();
-            System.out.println("------------- Displaying all customers -------------");
-            System.out.println();
+        System.out.println();
+        System.out.println("------------- Displaying all customers -------------");
+        System.out.println();
 
+        try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM customers");
 
@@ -47,6 +55,27 @@ public class Main {
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+        }
+    }
+
+    private static int getCustomerId(Connection connection, Customer customer) {
+        try {
+            Statement statement = connection.createStatement();
+            StringBuilder queryBuilder = new StringBuilder();
+            queryBuilder.append("SELECT * FROM customers WHERE first_name=");
+            queryBuilder.append("'").append(customer.getFirstName()).append("'");
+
+            ResultSet resultSet = statement.executeQuery(queryBuilder.toString());
+
+            while (resultSet.next()) {
+                return resultSet.getInt("customer_id");
+            }
+
+            return -1;
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return -1;
         }
     }
 
@@ -64,6 +93,37 @@ public class Main {
             queryBuilder.append("'").append(customer.getAddress()).append("'").append(", ");
             queryBuilder.append("'").append(customer.getCity()).append("'").append(", ");
             queryBuilder.append("'").append(customer.getState()).append("'").append(")");
+
+            int rowsAffected = statement.executeUpdate(queryBuilder.toString());
+
+            System.out.println(" rows affected: " + rowsAffected);
+
+        } catch (SQLException exception) {
+            System.out.println("Inserting customer throwed an exception: ");
+            System.out.println(exception.getLocalizedMessage());
+        }
+    }
+
+    private static void updateCustomer(Connection connection, Customer customer) {
+        System.out.println();
+        System.out.println("------------- Updating a customer -------------");
+        System.out.println();
+
+        try {
+            Statement statement = connection.createStatement();
+            StringBuilder queryBuilder = new StringBuilder();
+            queryBuilder.append("UPDATE customers SET first_name=");
+            queryBuilder.append("'").append(customer.getFirstName()).append("'").append(", ");
+            queryBuilder.append("last_name=");
+            queryBuilder.append("'").append(customer.getLastName()).append("'").append(", ");
+            queryBuilder.append("address=");
+            queryBuilder.append("'").append(customer.getAddress()).append("'").append(", ");
+            queryBuilder.append("city=");
+            queryBuilder.append("'").append(customer.getCity()).append("'").append(", ");
+            queryBuilder.append("state=");
+            queryBuilder.append("'").append(customer.getState()).append("' ");
+            queryBuilder.append("WHERE customer_id=");
+            queryBuilder.append("'").append(customer.getCustomerId()).append("' ");
 
             int rowsAffected = statement.executeUpdate(queryBuilder.toString());
 
